335ec812ddad1f5913efa66ee9ade46a65bf2b33
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-11-06 03:58:54 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,20 @@
-console.log('Hello world!')
-
-import Crawler from "crawler";
 import fs from "fs";
+import path from "path";
+import axios, { Axios } from "axios";
+import { pipeline } from "stream";
 
-const c = new Crawler({
-    encoding: null,
-    jQuery: false,
-    incomingEncoding: 'utf-8',
-    maxConnections : 10,
-    headers: {
-        'authority': 'forum.seedao.xyz',
-        'method': 'GET',
-        'path': '/api/thread/list?page=1&per_page=10&filter=all&category_index_id=19&tag_id=0&sort=latest&group_name=seedao',
-        'scheme': 'https',
+import * as stream from 'stream';
+import { promisify } from 'util';
+
+async function fetch(url: string): Promise<any> {
+    const headers = {
         'accept': 'application/json, text/plain, */*',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'en-US,en;q=0.9',
         'Api_key': 'metaforo_website',
         'Authorization': 'Bearer 20944|Sc8x9S1b3xIWksGsQyXwETv9edPGiSzRP7RB29mD',
         'Cookie': '_ga=GA1.1.313003853.1697860126; ARRAffinity=fce5f2795945f933a772f887ac42ae46802da36d6ace3b2c32343a83eabcca2e; _ga_QPVKNX8BXZ=GS1.1.1699079490.25.1.1699080762.0.0.0; _ga_FPJVR8J0T1=GS1.1.1699084107.10.1.1699086037.0.0.0',
-        'Referer':'https://forum.seedao.xyz/category/19',
+        'Referer': 'https://forum.seedao.xyz/category/19',
         'Sec-Ch-Ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
         'Sec-Ch-Ua-Mobile': '?0',
         'Sec-Ch-Ua-Platform': '"macOS"',
@@ -27,28 +22,88 @@ const c = new Crawler({
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-origin',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+    };
 
-    },
-    // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            // const $ = res.$;
-            // $ is Cheerio by default
-            //a lean implementation of core jQuery designed specifically for the server
-            // console.log($("body").text());
-            // console.log(Buffer.from(res.body.toString(), 'utf-8').toString());
-            // console.log(res);
-            fs.createWriteStream('./test.json').write(res.body);
+    const response = await axios.get(url, { headers: headers });
+    return response.data;
+}
+
+async function fetchThreadList(cateIdx: number, page: number, per_page: number): Promise<any> {
+    let url = `https://forum.seedao.xyz/api/thread/list?page=${page}&per_page=${per_page}&filter=category&category_index_id=${cateIdx}&tag_id=0&sort=new&group_name=seedao`;
+    return await fetch(url);
+}
+
+async function fetchThread(threadId: number): Promise<any> {
+    let url = `https://forum.seedao.xyz/api/get_thread/${threadId}?sort=new&group_name=seedao`;
+    return await fetch(url);
+}
+
+async function main() {
+
+    let threads = await fetchThreadList(12, 1, 20);
+
+    for (let i = 0; i < threads.data.threads.length; i++) {
+        let thread = threads.data.threads[i];
+        console.log(
+            thread.id,
+            thread.title,
+            thread.category_index_id,
+            thread.category_name,
+            thread.category_id,
+            thread.user.username,
+        );
+
+        let threadJson = await fetchThread(thread.id);
+        if (threadJson.data.thread.polls.length === 0) {
+            continue;
         }
-        done();
+
+        let poll = threadJson.data.thread.polls[0];
+        let pollId = poll.id;
+        let pollTitle = poll.title;
+        let pollThreadId = poll.thread_id;
+        let pollCreatedAt = poll.created_at;
+        let pollUpdatedAt = poll.updated_at;
+        let pollClosedAt = poll.close_at;
+        let pollStatus = poll.status;
+        let name = poll.name;
+        let is_nft = poll.is_nft;
+        let tokenAddress = poll.token_address;
+        let tokenId = poll.token_id;
+        let tokenType = poll.token_type;
+        let arweaveHash = poll.arweave;
+
+        console.log(pollId, pollTitle, pollThreadId, pollCreatedAt, pollUpdatedAt, pollClosedAt, pollStatus, name, is_nft, tokenAddress, tokenId, tokenType, arweaveHash);
+
+
+        if (tokenAddress == '0x9d34D407D8586478b3e4c39BE633ED3D7be1c80C' && tokenId == 4) {
+
+
+            // axios.get(`https://arweave.net/tx/${arweaveHash}/data.csv`, { responseType: 'blob' }).then(response => {
+            //     fs.writeFile(`./thread$-${thread.id}-${thread.title}.csv`, response.data, (err) => {
+            //         if (err) throw err;
+            //         console.log('The file has been saved!');
+            //     });
+            // });
+
+            const fileUrl = `https://arweave.net/tx/${arweaveHash}/data.csv`;
+            const fileName = `./thread-${thread.id}-${thread.title}.csv`;
+
+            console.log(`downloading ${fileUrl} to ${fileName}`);
+
+            const reponse = await axios.get(fileUrl, { responseType: 'stream' });
+            const writer = fs.createWriteStream(fileName);
+            reponse.data.pipe(writer);
+
+
+            console.log('download successful');
+        } else {
+            console.log('skip due to not P3.');
+        }
+
+
     }
-});
 
-// Queue just one URL, with default callback
-c.queue({uri: 'https://forum.seedao.xyz/api/thread/list?page=1&per_page=10&filter=all&category_index_id=19&tag_id=0&sort=latest&group_name=seedao'});
+}
 
-// Queue a list of URLs
-
-// c.queue(['http://www.google.com/','http://www.yahoo.com', 'https://forum.seedao.xyz/thread/p2-seedao-wiki-47099', 'https://forum.seedao.xyz/thread/p2-seedao-logo-seed-nft-46695']);
+main();
--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,25 @@
-import fs from "fs";
-import path from "path";
-import axios from "axios";
+console.log('Hello world!')
 
-async function fetch(url: string): Promise<any> {
-    const headers = {
+import Crawler from "crawler";
+import fs from "fs";
+
+const c = new Crawler({
+    encoding: null,
+    jQuery: false,
+    incomingEncoding: 'utf-8',
+    maxConnections : 10,
+    headers: {
+        'authority': 'forum.seedao.xyz',
+        'method': 'GET',
+        'path': '/api/thread/list?page=1&per_page=10&filter=all&category_index_id=19&tag_id=0&sort=latest&group_name=seedao',
+        'scheme': 'https',
         'accept': 'application/json, text/plain, */*',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'en-US,en;q=0.9',
         'Api_key': 'metaforo_website',
         'Authorization': 'Bearer 20944|Sc8x9S1b3xIWksGsQyXwETv9edPGiSzRP7RB29mD',
         'Cookie': '_ga=GA1.1.313003853.1697860126; ARRAffinity=fce5f2795945f933a772f887ac42ae46802da36d6ace3b2c32343a83eabcca2e; _ga_QPVKNX8BXZ=GS1.1.1699079490.25.1.1699080762.0.0.0; _ga_FPJVR8J0T1=GS1.1.1699084107.10.1.1699086037.0.0.0',
-        'Referer': 'https://forum.seedao.xyz/category/19',
+        'Referer':'https://forum.seedao.xyz/category/19',
         'Sec-Ch-Ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
         'Sec-Ch-Ua-Mobile': '?0',
         'Sec-Ch-Ua-Platform': '"macOS"',
@@ -18,45 +27,28 @@ async function fetch(url: string): Promise<any> {
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-origin',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-    };
 
-    const response = await axios.get(url, { headers: headers });
-    return response.data;
-}
+    },
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            // const $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            // console.log($("body").text());
+            // console.log(Buffer.from(res.body.toString(), 'utf-8').toString());
+            // console.log(res);
+            fs.createWriteStream('./test.json').write(res.body);
+        }
+        done();
+    }
+});
 
-async function main() {
+// Queue just one URL, with default callback
+c.queue({uri: 'https://forum.seedao.xyz/api/thread/list?page=1&per_page=10&filter=all&category_index_id=19&tag_id=0&sort=latest&group_name=seedao'});
 
-    let json, url;
+// Queue a list of URLs
 
-    url = 'https://forum.seedao.xyz/api/thread/list?page=1&per_page=10&filter=category&category_index_id=12&tag_id=0&sort=latest&group_name=seedao';
-    json = await fetch(url);
-    fs.writeFileSync('./list.json', JSON.stringify(json, null, 2));
-
-
-    url = 'https://forum.seedao.xyz/api/get_thread/45993?sort=old&group_name=seedao';
-    json = await fetch(url);
-
-    let thread = json.data.thread;
-    console.log(thread.id, thread.title, thread.user.username);
-
-    let poll = thread.polls[0];
-    let pollId = poll.id;
-    let pollTitle = poll.title;
-    let pollThreadId = poll.thread_id;
-    let pollCreatedAt = poll.created_at;
-    let pollUpdatedAt = poll.updated_at;
-    let pollClosedAt = poll.close_at;
-    let pollStatus = poll.status;
-    let name = poll.name;
-    let is_nft = poll.is_nft;
-    let tokenAddress = poll.token_address;
-    let tokenId = poll.token_id;
-    let tokenType = poll.token_type;
-    let arweaveHash = poll.arweave;
-
-    console.log(pollId, pollTitle, pollThreadId, pollCreatedAt, pollUpdatedAt, pollClosedAt, pollStatus, name, is_nft, tokenAddress, tokenId, tokenType, arweaveHash);
-
-    fs.writeFileSync('./thread.json', JSON.stringify(json, null, 2));
-}
-
-main();
+// c.queue(['http://www.google.com/','http://www.yahoo.com', 'https://forum.seedao.xyz/thread/p2-seedao-wiki-47099', 'https://forum.seedao.xyz/thread/p2-seedao-logo-seed-nft-46695']);
